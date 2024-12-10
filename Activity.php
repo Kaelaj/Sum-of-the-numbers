966d3252607f0266d855113dce31bd10ba53da59
Michaela Jane Sarro <michaelajane1291@gmail.com>
2024-12-09 15:21:42 -1200

    initial commit

---
+++ Activity.php
@@ -0,0 +1,11 @@
+<?php
+$numberOne = 20;
+$numberTwo = 10;
+
+$n = 0;
+while($n < 1) {
+    $sum = $numberOne + $numberTwo;
+    echo "The sum of the number $numberOne and $numberTwo is $sum";
+    $n++;
+}
+?>
